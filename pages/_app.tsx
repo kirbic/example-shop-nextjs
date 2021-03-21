19ea1cd241ba9f49361de6f789a136cc21fe2a78
Marc Pomar <marc@corecode.school>
2021-03-21 21:53:01 +0100

    fix: ssr session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 // pages/_app.js
 import React from "react";
-import { UserProvider } from "@kirbic/nextjs";
+import { UserProvider, getAccessToken, getSession } from "@kirbic/nextjs";
 import { CartContainer } from "@kirbic/react";
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+  console.log(pageProps);
   return (
     <UserProvider>
       <CartContainer shop_id={process.env.NEXT_PUBLIC_KIRBIC_SHOP}>
@@ -12,3 +13,15 @@ export default function App({ Component, pageProps }) {
     </UserProvider>
   );
 }
+
+App.getInitialProps = async ({ ctx: { req, res } }) => {
+  const session = getSession(req, res);
+  console.log("session");
+  console.log(session);
+  const token = getAccessToken(req, res);
+  console.log("token");
+  console.log(token);
+  return {};
+};
+
+export default App;
